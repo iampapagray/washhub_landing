91729c4b3bfe240a28eb79dedceb24aab19bf600
Graham Bryan <iampapagray@gmail.com>
2021-01-11 14:18:12 +0000

    feedback Section complete

--- src/widgets/Feedback.vue
+++ src/widgets/Feedback.vue
@@ -1,10 +1,60 @@
 <template>
-    <div id="feedback" class="full-height primary-bg">
+    <div id="feedback" class=" primary-bg">
        <b-container fluid>
            <b-row id="feedBack">
                <b-container>
-                   <b-row class="feedBackRow">
-                        jkj
+                   <b-row class="feedBackRow feedbackHeight" align-v="center">
+                        <b-col cols="4" class="text-left ">
+                            <div class="imageBox">
+                                <b-img :src="require('@/assets/bubble.svg')"  class="bubbleIcon" fluid alt="bubble icon" ></b-img>
+                            </div>
+                            <div class="innerBox">
+                                <h3 class="subHeading">
+                                    What People Are <span class="alt-color">Saying</span>
+                                </h3>
+                            </div>
+                            
+                        </b-col>
+                        <b-col cols="4" class="text-left">
+                            <div class="feedBox">
+                                <div class="top"></div>
+                                <div class="bottom"></div>
+                                <div class="content d-flex align-items-center justify-content-center">
+                                    <div class="box px-1">
+                                        <b-avatar size="4rem" :src="require('@/assets/person.jpg')"></b-avatar>
+                                        <p class="feedbackText">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                                            Similique aperiam sapiente maiores quod cum officiis, 
+                                            accusan suscipit sunt impedit! Vel, ratione error?
+                                        </p>
+                                        <b-img :src="require('@/assets/5star.svg')"  class="ratings" fluid alt="5-stars" ></b-img>
+                                        <h6 class="feedbackName">
+                                            Steve Smith <span class="name-alt">from Austin</span>
+                                        </h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </b-col>
+                        <b-col cols="4" class="text-left">
+                            <div class="feedBox">
+                                <div class="top"></div>
+                                <div class="bottom"></div>
+                                <div class="content d-flex align-items-center justify-content-center">
+                                    <div class="box px-1">
+                                        <b-avatar size="4rem" :src="require('@/assets/person.jpg')"></b-avatar>
+                                        <p class="feedbackText">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                                            Similique aperiam sapiente maiores quod cum officiis, 
+                                            accusan suscipit sunt impedit! Vel, ratione error?
+                                        </p>
+                                        <b-img :src="require('@/assets/4star.svg')"  class="ratings" fluid alt="5-stars" ></b-img>
+                                        <h6 class="feedbackName">
+                                            Kristen Lynn <span class="name-alt">from Ohio</span>
+                                        </h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </b-col>
                    </b-row>
                </b-container>
            </b-row>
@@ -27,7 +77,49 @@ export default {
 
 <style scoped>
     #feedBack {
-        min-height: 42.5vh;
+        min-height: 63.75vh;
+    }
+    .feedBox {
+        height: 40vh;
+        width: 70%;
+        background-color: var(--deep);
+        border-radius: 16px;
+    }
+    .top{
+        height: 8vh;
+    }
+    .bottom{
+        height: 32vh;
+        width: 100%;
+        background-color: white;
+        border-bottom-left-radius: 16px;
+        border-bottom-right-radius: 16px;
+    }
+    .content{
+        z-index: inherit;
+        text-align: center;
+        position: absolute;
+        top: 0;
+        height: 40vh;
+        width: 64%;
+    }
+    .feedbackText {
+        padding-top: 4vh;
+        font-size: 0.6rem;
+    }
+    .feedbackName {
+        font-size: 0.7rem;
+        color: var(--deep);
+        font-weight: 600;
+        margin-top: 1vh;
+    }
+    .name-alt {
+        color: var(--deep);
+        font-weight: 400;
+    }
+    .ratings {
+        width:40%;
+        margin-top: 3vh;
     }
     .feedBackRow {
         padding-left:5%;
@@ -35,11 +127,21 @@ export default {
     }
     .subHeading{
         font-weight: 700;
-        color: var(--deep);
+        color: white;
     }
     .headingDesc{
         font-weight: 600;
         color: var(--grey);
+    }
+    .innerBox {
+        width: 60%;
+        margin-left: 20%;
+    }
+    .imageBox{
+        width: 18%;
+        margin-left: 65%;
+        align-items: flex-end;
+        justify-content: end;
     }
     .desc {
         font-weight: 600;
@@ -54,11 +156,11 @@ export default {
         color: var(--deep);
         font-weight: 600;
     }
-    .pricingHead {
-        width: 12vw;
-        text-align: left;
+    .feedbackHeight {
+        min-height: 63.75vh;
     }
-    .pricingHeight {
-        min-height: 42.5vh;
+    .bubbleIcon {
+        height: 80px;
+        width: 80px;
     }
 </style>
